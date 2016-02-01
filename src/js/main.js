7718e6fc9862d4a42c0f8d31c4f3fd067d6e77eb
DeepenLau <417520940@qq.com>
2016-02-01 17:38:02 +0800

    [新增]独立首页，搭建二级页面布局基本布局

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var $bgWrap = $('#bg-wrap');
+var $bodyBgWrap = $('#body-bg-wrap');
 
 
 //首页
@@ -7,7 +7,60 @@ if ($('#index').length) {
         scalarX: 10,
         scalarY: 10
     });
-    $('#bg-wrap').parallax({
+    $bodyBgWrap.parallax({
+        calibrateX: false,
+        calibrateY: false,
+        invertX: false,
+        invertY: false,
+        limitX: false,
+        limitY: false,
+        scalarX: 5,
+        scalarY: 5
+    });
+    indexResize();
+    function indexResize() {
+        $('.hover-text').css('left', '54px');
+        $bodyBgWrap.find('.bamboo-1').css({
+            'right': '-50px',
+            'top': '-25px',
+            'left': 'auto'
+        });
+        $bodyBgWrap.find('.bamboo-2').css({
+            'right': '-50px',
+            'top': '-25px',
+            'left': 'auto'
+        });
+        $bodyBgWrap.find('.dragon').css({
+            'top': 'auto',
+            'bottom': '-20%',
+            'left': '-8%'
+        });
+        $bodyBgWrap.find('.title').css({
+            'top': '20%',
+            'left': '50%',
+            'marginLeft': '-93px'
+        });
+        $bodyBgWrap.find('.cloud-1').css({
+            'top': '50%',
+            'left': '50%',
+            'marginLeft': '-120px',
+            'marginTop': '80px'
+        });
+        $bodyBgWrap.find('.cloud-2').css({
+            'top': '50%',
+            'left': '50%',
+            'marginTop': '120px'
+        });
+        $bodyBgWrap.find('.bird').css({
+            'top': '18%',
+            'left': '30%'
+        })
+    };
+}
+
+//细语倾诉
+if ($('#history').length) {
+    $bodyBgWrap.parallax({
         calibrateX: false,
         calibrateY: false,
         invertX: false,
@@ -19,39 +72,36 @@ if ($('#index').length) {
     });
     res();
     function res() {
-        $('.hover-text').css('left', '54px');
-        $bgWrap.find('.bamboo-1').css({
+        $bodyBgWrap.find('.bamboo-1').css({
             'right': '-50px',
             'top': '-25px',
             'left': 'auto'
         });
-        $bgWrap.find('.bamboo-2').css({
+        $bodyBgWrap.find('.bamboo-2').css({
             'right': '-50px',
             'top': '-25px',
             'left': 'auto'
         });
-        $bgWrap.find('.dragon').css({
-            'top': 'auto',
-            'bottom': '-20%',
-            'left': '-8%'
-        })
-        $bgWrap.find('.title').css({
+        $bodyBgWrap.find('.dragon').css({
+            'left': '-200px'
+        });
+        $bodyBgWrap.find('.title').css({
             'top': '20%',
             'left': '50%',
             'marginLeft': '-93px'
-        })
-        $bgWrap.find('.cloud-1').css({
+        });
+        $bodyBgWrap.find('.cloud-1').css({
             'top': '50%',
             'left': '50%',
             'marginLeft': '-120px',
             'marginTop': '80px'
-        })
-        $bgWrap.find('.cloud-2').css({
+        });
+        $bodyBgWrap.find('.cloud-2').css({
             'top': '50%',
             'left': '50%',
             'marginTop': '120px'
-        })
-        $bgWrap.find('.bird').css({
+        });
+        $bodyBgWrap.find('.bird').css({
             'top': '18%',
             'left': '30%'
         })
